ff3ee72232b3ed28c2ac968d473b5a1f2f4953d4
tracewilson <wiltrac3@byu.edu>
2025-01-27 10:54:33 -0700

    test3

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World! Test");
+Console.WriteLine("Hello, World! Test Test test");
 
 Console.ReadLine();
